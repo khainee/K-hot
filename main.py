476988a0b25072a9f630c609a6b6eb18be84145c
Mr Khaine <118953358+khainee@users.noreply.github.com>
2024-03-22 22:43:48 +0630

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import urllib.parse
 API_URL = 'https://api0.herewallet.app/api/v1/user/hot/claim'
 HEADERS = {
     'Host': 'api0.herewallet.app',
-    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjUyMTQxMTAsImRpZCI6ODQ1OTYzOCwiZGV2aWNlIjpudWxsLCJhY2NvdW50X2lkIjoiaTY0NTkwOTc0NDAudGciLCJkZXZpY2VfaWQiOiIwNWJlMjFmOS02Zjg3LTQ1NTYtYWVjNS02MWNkYjNkMjA2YTAiLCJwbGF0Zm9ybSI6InRlbGVncmFtIiwidGltZXN0YW1wIjoxNzEwODMyMjI0LjAsInZpZXdfb25seSI6ZmFsc2V9.oFroXS98xIOMaNxPCjDQtRT-7pxQcPwjiFGWnOyjPtk',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2010J19SI Build/RKQ1.201004.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.119 Mobile Safari/537.36',
     'Content-Type': 'application/json',
     'Network': 'mainnet',
@@ -15,7 +14,8 @@ HEADERS = {
 }
 
 def claim_K():
-    headers = HEADERS.copy()  # Copy headers to avoid modifying the original dictionary
+    headers = HEADERS.copy()
+    headers['Authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjUyMTQxMTAsImRpZCI6ODQ1OTYzOCwiZGV2aWNlIjpudWxsLCJhY2NvdW50X2lkIjoiaTY0NTkwOTc0NDAudGciLCJkZXZpY2VfaWQiOiIwNWJlMjFmOS02Zjg3LTQ1NTYtYWVjNS02MWNkYjNkMjA2YTAiLCJwbGF0Zm9ybSI6InRlbGVncmFtIiwidGltZXN0YW1wIjoxNzEwODMyMjI0LjAsInZpZXdfb25seSI6ZmFsc2V9.oFroXS98xIOMaNxPCjDQtRT-7pxQcPwjiFGWnOyjPtk'
     headers['Telegram-Data'] = 'user=%7B%22id%22%3A6459097440%2C%22first_name%22%3A%22K%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-7410836090700338256&chat_type=sender&auth_date=1710832328&hash=3b83294a563569d35240aaa05893214a750cd24c9992efebe47da36f53c2b1ff'
     DATA = {
     "game_state": {
@@ -36,9 +36,12 @@ def claim_K():
         PAYLOAD['game_state']['balance'] = response_data.get('hot_in_storage', PAYLOAD['game_state']['balance'])
         print(f"Claim Success.Current Balance is {int(PAYLOAD['game_state']['balance']) / 1000000}")
         k_sleep = PAYLOAD['game_state']['storage'] * 6 * 60
-        time.sleep(k_sleep)
+        time.sleep(7200)
     else:
         print(response_data['detail'])
+
+time_k = time.time()
+time_m = time.time()
 
 while True:
     claim_K()
